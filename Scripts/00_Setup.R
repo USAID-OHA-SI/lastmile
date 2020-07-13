e158235bf7bd48ffb9b5f711f52117fd16c668bf
Baboyma Kagniniwa <baboyma@gmail.com>
2020-07-13 16:28:24 -0400

    Setting up project structure

---
+++ Scripts/00_Setup.R
@@ -0,0 +1,31 @@
+##
+## PROJECT:  PEPFAR Geospatial Analytics
+## AUTHOR:   B.Kagniniwa, G.Sarfaty | USAID
+## LICENSE:  MIT
+## PURPOSE:  Store Project References
+## Date:     2020-07-13
+
+# LIBRARIES
+
+library(here)
+
+# DIRECTORIES -----------------------
+
+## Data
+
+    dir_data <- "./Data"
+    dir_dataout <- "./Dataout"
+
+    dir_geo <- "../../GEODATA"
+    dir_mer <- "../../MERDATA"
+    dir_datim <- "../../HFR-Data/Datim"
+
+    file_ovc <- "OVC DREAMS YCM Districts_Sites for COP20.xlsx"
+    file_psnu_ims <- "MER_Structured_Datasets_PSNU_IM_FY18-20_20200605_v1_1.rds"
+
+
+## IMAGES / OUTPUTs ------------------
+
+    dir_img <- "./Images"
+    dir_graphs <- "./Graphics"
+
