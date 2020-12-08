71f9c1fcb15acaca87482fa2309a5e462c153fb1
Aaron Chafetz <achafetz@usaid.gov>
2020-12-08 11:16:26 -0500

    map updates

--- Scripts/99_FY20Q4_POART_NGA_ad-hoc_unmet-need.R
+++ Scripts/99_FY20Q4_POART_NGA_ad-hoc_unmet-need.R
@@ -27,25 +27,22 @@
 
   # folder_setup()
 
-  source("Scripts/00_Geo_Utilities.R")
-
 # LOAD DATA ---------------------------------------------------------------
 
   df <- read_excel("Data/EpiData_2020.xlsx")
+
+  usaid_states <- read_excel("Data/EpiData_2020.xlsx", "USAID sites") %>%
+    pull(PSNU)
 
 # SPATIAL DATA ------------------------------------------------------------
 
   nga_0 <- get_admin0("Nigeria")
   nga_1 <- get_admin1("Nigeria")
 
-  nga_neighbors <- geo_neighbors("Nigeria")
-
-
   nga_1 <- nga_1 %>%
     mutate(state = recode(name,
                           "Federal Capital Territory" = "FCT",
                           "Nassarawa" = "Nasarawa"))
-
 
 # MUNGE -------------------------------------------------------------------
 
@@ -64,16 +61,10 @@
                 values_from = value)
 
   df <- df %>%
-    group_by(state) %>%
-    mutate(cov_deta = art_coverage/lag(art_coverage)) %>%
-    ungroup()
-
+    mutate(usaid_state = state %in% usaid_states)
 
   df_geo <- df %>%
     full_join(nga_1, by = "state")
-
-
-
 
 
 # MAP ---------------------------------------------------------------------
@@ -84,48 +75,37 @@
     ggplot() +
     geom_sf(aes(fill = unmet_need, geometry = geometry),
             color = "gray90") +
-    # geom_text_repel(data = df_geo, aes(label = state)) +
     scale_fill_si("moody_blues", discrete = FALSE, label = comma, reverse = TRUE, na.value = "grey90") +
     labs(title = "PLHIV") +
     si_style_map()
 
   v_cov <- df_geo %>%
+    mutate(usaid_art_coverage = case_when(usaid_state == TRUE ~ art_coverage),
+           usaid_state = case_when(usaid_state == TRUE & period == "FY20" ~ state),
+           period = "FY20") %>%
     ggplot() +
-    geom_sf(aes(fill = art_coverage, geometry = geometry),
-            color = "gray90") +
-    facet_wrap(~period) +
+    geom_sf(aes(fill = usaid_art_coverage, geometry = geometry),
+            color = "gray90", size = .4) +
+    geom_text(aes(longitude, latitude, label = usaid_state), colour = "white", na.rm = TRUE,
+              family = "Source Sans Pro", size = 2) +
+    # facet_wrap(~period, ncol = 1) +
     scale_fill_si(discrete = FALSE, label = percent, reverse = TRUE, na.value = "grey70") +
-    labs(title = "ART Coverage") +
-    si_style_map()
-
-
-  # v_dot <- df %>%
-  #   filter(!is.na(art_coverage)) %>%
-  #   mutate(order = ifelse(period == "FY20", art_coverage, 0),
-  #          start = case_when(period == "FY19" ~ art_coverage),
-  #          end = case_when(period == "FY20" ~ art_coverage)) %>%
-  #   ggplot(aes(art_coverage, fct_reorder(state, order, sum), group = state)) +
-  #   geom_path() +
-  #   geom_point(aes(start), shape = 21, fill = "white", na.rm = TRUE) +
-  #   geom_point(aes(end), size = 2, shape = 21, fill = "gray30", na.rm = TRUE) +
-  #   expand_limits(x = 0) +
-  #   scale_x_continuous(labels = percent) +
-  #   labs(x = NULL, y = NULL,
-  #        title = "Change in ART Coverage") +
-  #   si_style()
-
+    si_style_map() +
+    theme(strip.text.x = element_text(size = "12", face = "bold"))
 
   v_dot <- df %>%
-    filter(!is.na(art_coverage)) %>%
+    filter(!is.na(art_coverage),
+           usaid_state == TRUE) %>%
     mutate(order = ifelse(period == "FY20", art_coverage, 0),
            start = case_when(period == "FY19" ~ art_coverage),
            end = case_when(period == "FY20" ~ art_coverage),
-           pd_lab = case_when(state == "FCT" ~ period)) %>%
+           pd_lab = case_when(state == "Cross River" ~ period)) %>%
     group_by(state) %>%
     mutate(min = case_when(art_coverage == min(art_coverage) ~ art_coverage),
            max = case_when(art_coverage == max(art_coverage) ~ art_coverage)) %>%
     ungroup() %>%
     ggplot(aes(art_coverage, fct_reorder(state, order, sum), group = state)) +
+    geom_blank(aes(art_coverage * 1.05)) +
     geom_path() +
     geom_point(aes(start), shape = 21, fill = "white", na.rm = TRUE) +
     geom_text(aes(min, label = glue("{state}  {percent(art_coverage, 1)}")),
@@ -135,45 +115,28 @@
     geom_text(aes(label = pd_lab),
               size = 1.5, vjust = -1.2, family = "Source Sans Pro", na.rm = TRUE) +
     geom_point(aes(end), size = 2, shape = 21, fill = "gray30", na.rm = TRUE) +
-    expand_limits(x = 0, y = 38) +
+    expand_limits(x = 0, y = (length(usaid_states) + 1)) +
     scale_x_continuous(labels = percent) +
-    labs(x = NULL, y = NULL #, title = "Change in ART Coverage"
-         ) +
-    si_style_void() #+
-    # theme(legend.key.size = )
+    labs(x = NULL, y = NULL) +
+    si_style_void()
 
-
-  ((v_plhiv + v_cov)/v_dot) +
-    plot_layout(widths = c(1, 2))
-
-  si_save("Images/NGA_art-coverage.png")
-
-
-
-  v_cov <- df_geo %>%
-    ggplot() +
-    geom_sf(aes(fill = art_coverage, geometry = geometry),
-            color = "gray90", size = .4) +
-    facet_wrap(~period, ncol = 1) +
-    scale_fill_si(discrete = FALSE, label = percent, reverse = TRUE, na.value = "grey70") +
-    # labs(title = "ART Coverage") +
-    si_style_map()
 
 
   (v_cov + v_dot) +
-    plot_layout(widths = c(1, 2)) +
+    # plot_layout(widths = c(1, 2)) +
     plot_annotation(
-      title = 'CHANGE IN ART COVERAGE ',
-      subtitle = 'Nigeria FY19-20'
-    ) & si_style_map() + theme(panel.grid = element_blank(),
+      subtitle = 'Nigeria ART Coverage Across USAID Supported States | FY20',
+      caption = "Notes: ART Coverage = Current on Treatment / PLHIV\nOnly one point estimate for PLHIV, both year's calculations use the same PLHIV value\nSource: NAIIS estimates from Spectrum Projection, January 2020"
+      ) & si_style_map() + theme(panel.grid = element_blank(),
                                plot.title = element_text(hjust = 0),
                                plot.subtitle = element_text(hjust = 0),
                                legend.key.width = unit(.5,"in"),
-                               legend.key.height = unit(.1,"in")
+                               legend.key.height = unit(.1,"in"),
+                               plot.caption = element_text(hjust = 1)
                                )
 
 
 
-  si_save("Images/NGA_art-coverage_v2.png")
+  si_save("Images/NGA_art-coverage_v3.png")
 
 
