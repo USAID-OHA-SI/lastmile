ea367f839a03f3d77a467428d6f1fa7bb3debd3d
Baboyma Kagniniwa <baboyma@gmail.com>
2021-03-02 10:14:50 -0500

    DREAMS -update

--- Scripts/06_GAS_OU_AGYW_PREV_Completion.R
+++ Scripts/06_GAS_OU_AGYW_PREV_Completion.R
@@ -6,7 +6,6 @@
 ##  UPDATED: 2020-11-06
 
 # Libraries
-library(extrafont)
 library(tidyverse)
 library(sf)
 library(glitr)
@@ -14,6 +13,7 @@ library(gisr)
 library(here)
 library(scales)
 library(patchwork)
+library(extrafont)
 library(ICPIutilities)
 
 
@@ -29,17 +29,17 @@ library(ICPIutilities)
     dir_data <- here("Data")
     dir_dataout <- here("Dataout")
 
-    dir_geo <- "C:/Users/gsarfaty/Documents/GIS/DATA/Boundaries"
-    dir_terr <- "C:/Users/gsarfaty/Documents/GIS/DATA/Topography"
-    dir_merdata <- "C:/Users/gsarfaty/Documents/DATIM"
+    dir_geo <- si_path("path_vector")
+    dir_terr <- si_path("path_raster")
+    dir_merdata <- si_path("path_msd")
 
     dir_img <- here("Images")
     dir_graphs <- here("Graphics")
 
   ## Reporting vars
 
-    rep_fy <- 2020
-    rep_qtr <- 4
+    rep_fy <- 2021
+    rep_qtr <- 1
     rep_pd <- paste0("FY", str_sub(rep_fy, 3, 4), "Q", rep_qtr)
     curr_pd <- paste0("qtr", rep_qtr)
     remv_pd <- paste0("qtr", 1:4)
@@ -47,10 +47,24 @@ library(ICPIutilities)
 
   ## Output Caption
 
-    caption <- "Reporting Period: FY20Q4; Data Source: FY20Q4i MSD"
+    caption <- "Reporting Period: FY21Q1; Data Source: FY21Q1i MSD"
 
-  ## overwrite paths with your own.
-    source("../_setup/00_Setup.R")
+  ## MER Data - get the latest MSD PSNU x IM file
+
+    file_psnu_im <- return_latest(
+        folderpath = dir_merdata,
+        pattern = "^MER_.*_PSNU_IM_.*_20210212_v1_1.zip$",
+        recursive = FALSE
+      )
+
+  ## Shapefile path
+    file_shp <- return_latest(
+      folderpath = dir_geo,
+      pattern = "VcPepfarPolygons.*.shp",
+      recursive = TRUE
+    )
+
+    file_shp
 
 # FUNCTIONS ----------------------------------------------
 
@@ -121,34 +135,26 @@ library(ICPIutilities)
 
 # DATA ---------------------------------------------------
 
-  # MER Data - get the latest MSD PSNU x IM file
-  # Q4 file => MER_Structured_Datasets_PSNU_IM_FY18-21_20201113_v2_1.zip
-  file_psnu_im <- list.files(
-      path = dir_merdata,
-      pattern = "^MER_.*_PSNU_IM_.*_20201113_v1_1.zip$",
-      recursive = FALSE,
-      full.names = TRUE
-    ) %>%
-    sort() %>%
-    last()
-
   # Read data
   df <- read_msd(file_psnu_im)
-
-  # PEPFAR GEO Data
-  spdf <- build_spdf(
-    dir_geo = paste0(dir_geo, "/PEPFAR/VcPepfarPolygons_2020.07.24"),
-    name = "VcPepfarPolygons.shp",
-    df_psnu = df
-  )
 
   # Terrain raster file
   terr <- get_raster(terr_path = dir_terr)
 
+  # PEPFAR GEO Data
+  spdf_pepfar <- file_shp %>% sf::read_sf()
+
+  df_attrs <- gisr::get_ouuids() %>%
+    filter(!str_detect(operatingunit, " Region$")) %>%
+    pull(operatingunit) %>%
+    map_dfr(.x, .f = ~get_attributes(country = .x))
+
+  spdf_pepfar <- spdf_pepfar %>%
+    left_join(df_attrs, by = c("uid" = "id"))
+
   # DREAMS - AGYW Prevalence
   df_agyw <- extract_agyw_prevalence(
     df_msd = df,
-    country = NULL,
     rep_fy = rep_fy,
     rep_qtr = rep_qtr
   )
